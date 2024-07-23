d23f86f4fbec841563fde154fc94f6de11289881
Sam Perlmutter <sam.perlmutter227@gmail.com>
2024-07-23 19:41:46 -0400

    Change google java format to aosp and apply

--- src/test/java/frc/robot/ExampleTest.java
+++ src/test/java/frc/robot/ExampleTest.java
@@ -1,17 +1,15 @@
 package frc.robot;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class ExampleTest
-{
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
     // To learn more about how to write unit tests, see the
     // JUnit 5 User Guide at https://junit.org/junit5/docs/current/user-guide/
 
     @Test
-    void twoPlusTwoShouldEqualFour()
-    {
+    void twoPlusTwoShouldEqualFour() {
         assertEquals(4, 2 + 2);
     }
 }
